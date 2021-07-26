297b095d97c9cbefbb369e179b41953a6f166556
EstherMugisha <86796790+EstherMugisha@users.noreply.github.com>
2021-07-25 20:03:09 -0500

    added asciianimation

---
+++ ASCIIanimation/ascii.js
@@ -0,0 +1,112 @@
+let frame;
+let currentAnime;
+let speed=250;
+
+var timer=setInterval(displayAnimation,speed);
+window.onload=function(){
+'strict';
+document.getElementById("start").onclick=startAnimation;
+document.getElementById("animation").onchange=choseAnimation;
+document.getElementById("stop").onclick=stopAnimation;
+document.getElementById("fontsize").onchange=changeFont;
+document.getElementById("turbo").onclick=turbo;
+};
+
+function choseAnimation(){
+let anime=document.getElementById("animation");
+let textArea=document.getElementById("text-area");
+switch(anime.selectedIndex){
+    case 0:
+        textArea.value=BLANK;
+        break;
+    case 1:
+        textArea.value=EXERCISE;
+        break;
+    case 2:
+        textArea.value=JUGGLER;
+        break;
+    case 3:
+        textArea.value=BIKE;
+        break;
+    case 4:
+        textArea.value=DIVE;
+        break;  
+}  
+}
+function startAnimation(){
+    if(document.getElementById("animation".selectedIndex)!=0){
+        document.getElementById("start").disabled = true; 
+        document.getElementById("stop").disabled = false;
+        document.getElementById("animation").disabled=true;
+        let textArea = document.getElementById("text-area");
+        currentAnime=textArea.value.split("=====");
+        frame=0;
+        clearInterval(timer);
+        timer = setInterval(displayAnimation,speed);
+
+    }
+}
+
+function displayAnimation(){
+    let textArea = document.getElementById("text-area");
+    textArea.value = currentAnime[frame];
+    frame++;
+    if(frame == currentAnime.length){
+        frame=0;
+    }
+}
+
+function stopAnimation(){
+    document.getElementById("stop").disabled=true;
+    document.getElementById("start").disabled=false;
+    document.getElementById("animation").disabled=false;
+    clearInterval(timer);
+    choseAnimation();
+}
+
+function changeFont(){
+    let size=document.getElementById("fontsize");
+    let textArea=document.getElementById("text-area");
+    switch(size.selectedIndex){
+        case 0:
+            textArea.style.fontSize = "7pt";
+            break;
+        case 1:
+            textArea.style.fontSize = "10pt";
+            break;
+        case 2:
+            textArea.style.fontSize = "12pt";
+            break;
+        case 3:
+            textArea.style.fontSize ="16pt";
+            break;
+        case 4:
+            textArea.style.fontSize ="24pt";
+            break;
+        case 5:
+            textArea.style.fontSize ="32pt";
+            break;
+    }
+}
+
+function turbo(){
+    let sp=document.getElementById("turbo");
+    clearInterval(timer);
+    if(sp.checked==true){
+        speed=50;
+        timer=setInterval(displayAnimation,speed);
+    }
+    else{
+        speed=250;
+        timer=setInterval(displayAnimation,speed);
+    }
+}
+
+
+
+
+
+
+
+
+
