86ca37384b66c34e29560b40a20762187939f66a
EstherMugisha <86796790+EstherMugisha@users.noreply.github.com>
2021-07-27 08:18:05 -0500

    something

--- W2D1/datevalid.js
+++ W2D1/datevalid.js
@@ -1,16 +1,18 @@
 window.onload = eventHandler;
 
 function eventHandler() {
+    "use strict";
     document.getElementById("addbtn").onclick = addProduct;
 }
 
 function addProduct() {
-    var dateBox = document.getElementById("txtDate");
-    var datereg = new RegExp(/^([1-2][0-9][5-9][0-9]|20[0-4][0-9]|2050)[-](0?[1-9]|1[0-2])[-](0?[1-9]|[12][0-9]|3[01])$/);
+    var date = document.getElementById("txtDate");
+    var datereg = new RegExp(/^(1[5-9][0-9][0-9])|((20[0-4][0-9])|2050)[-](0?[1-9]|1[0-2])[-](0?[1-9]|[12][0-9]|3[01])$/);
 
-    if(datereg.exec(dateBox.value)){
+    if(datereg.exec(date.value)){
         alert("Success!")
     }else {
-        alert("Please Enter a Date in the format of yyyy-mm-dd")
+        alert("Please Enter a Date between the year of 1500 and 2050 in the format of yyyy-mm-dd")
     }
 }
+
