ebaa05872a9e992909acdd1eb8886eafdd8edf29
EstherMugisha <86796790+EstherMugisha@users.noreply.github.com>
2021-07-27 22:01:40 -0500

    added stuff

---
+++ W2D2/closure.js
@@ -0,0 +1,36 @@
+var x=1;
+var a=5;
+var b=10;
+var c=function(a,b,c){
+    console.log(x);
+    console.log(a);
+    var f=function(a,b,c){
+        b=a;
+        console.log(b);
+        b=c;
+        var x=5;
+    }
+    f(a,b,c);
+    console.log(b);
+    var x=10;
+    }
+c(a,b,c);
+console.log(b);
+console.log(x);
+
+var foo=1;
+function bar(){
+    if(!foo){
+        var foo=10;
+    }
+    alert(foo);
+}
+bar();
+
+var z=9;
+function myFunction(){
+    return z*z;
+}
+console.log(myFunction());
+z=5;
+console.log(myFunction())
